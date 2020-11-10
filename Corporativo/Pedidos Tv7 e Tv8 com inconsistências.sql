e7d72cd5de3fba98fe65e5e87f71ee0b1667248a
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2020-11-10 17:07:51 -0300

    Update

---
+++ Corporativo/Pedidos Tv7 e Tv8 com inconsistências.sql
@@ -0,0 +1,31 @@
+SELECT C.DATA,
+       C.NUMPED,
+       C.CONDVENDA,
+       C.CONTAORDEM,
+       C.CODCLI,
+       T.CLIENTE,
+       C.CODCOB,
+       C.CODEMITENTE
+  FROM PCPEDC C
+  LEFT JOIN PCCLIENT T ON C.CODCLI = T.CODCLI
+ WHERE C.POSICAO NOT IN (
+    'F', 'C'
+)
+   AND C.CONDVENDA IN (
+    7, 8
+)
+   AND (C.CONTAORDEM IS NULL
+    OR (
+    CASE
+        WHEN (C.CONDVENDA = 8
+           AND C.CODCOB = 'SENT') THEN 1
+        ELSE 0
+    END
+) = 0
+   AND (
+    CASE
+        WHEN (C.CONDVENDA = 7
+           AND C.CODCOB <> 'SENT') THEN 1
+        ELSE 0
+    END
+) = 0)
