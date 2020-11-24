2cb0665ef5e2cee102a7557049380baf0bf6c9a7
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2020-11-23 22:44:07 -0300

    Create VIEW_JC_CONTAS_PAGAR.sql

---
+++ Views/Contas a Pagar/VIEW_JC_CONTAS_PAGAR.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE VIEW VIEW_JC_CONTAS_PAGAR AS
+    SELECT L.CODFILIAL,
+           L.RECNUM,
+           L.DTEMISSAO,
+           L.DTVENC,
+           L.DTPAGTO,
+           L.CODCONTA,
+           L.CODFORNEC,
+           L.TIPOPARCEIRO,
+           L.HISTORICO,
+           (L.NUMNOTA || ' - ' || L.DUPLIC) AS DUPLICATA,
+           L.TIPOLANC,
+           L.VALOR,
+           (NVL (L.VALOR, 0) - NVL (L.DESCONTOFIN, 0) - NVL (L.VALORDEV, 0)) AS VALORLIQ,
+           TRIM (L.HISTORICO2) AS HISTORICO2
+      FROM PCLANC L
+     WHERE L.DTPAGTO IS NULL
+       AND L.DTCANCEL IS NULL;
+/
