fbf010568992bf988cc28c5d893db2b8336b6a7a
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2021-02-04 17:48:52 -0300

    Update VIEW_JC_LANC_PAGOS.sql
    Retirada duplicidade de lançamentos de desconto financeiro, aonde havia aplicação de mais de um desconto sobre a mesma duplicata (utilizado GROUP BY para resolver)

--- Views/Contas a Pagar/VIEW_JC_LANC_PAGOS.sql
+++ Views/Contas a Pagar/VIEW_JC_LANC_PAGOS.sql
@@ -294,37 +294,58 @@ CREATE OR REPLACE VIEW VIEW_JC_LANC_PAGOS AS
      /*RETIRAR LANÇAMENTOS DE VERBAS COM ORIGEM EM DEVOLUCAO A FORNECEDOR RECEBIDAS COMO DINHEIRO*/
     )
 /*--DESCONTOS FINACEIROS OBTIDOS--*/
-    SELECT B.CODFILIAL,
-           B.RECNUM,
-           B.VPAGO,
-           L.DESCONTOFIN AS VALOR,
-           B.DTCOMPETENCIA,
-           B.DTPAGTO,
-           B.DTCOMPENSACAO,
-           B.CODFILIALBANCO,
-           B.CODBANCO,
-           B.CODCONTABILBANCO,
-           B.CODCONTA,
-           B.GRUPOCONTA,
-           B.CODFORNEC,
-           '' AS CODCONTABCLIENTE,
-           B.TIPOPARCEIRO,
-           B.NUMTRANS,
-           'D' AS TIPO,
-           (
-               CASE
-                   WHEN L.DUPLIC IS NULL
-                       OR L.DUPLIC = '0' THEN 'DESC FIN Nº ' || L.NUMNOTA || ' - ' || L.HISTORICO
-                   ELSE 'DESC FIN Nº ' || L.NUMNOTA || '-' || L.DUPLIC || ' - ' || L.HISTORICO
-               END
-           ) HISTORICO
-      FROM PCLANC L
-     INNER JOIN BASEPCLANC B ON L.RECNUM = B.RECNUM
-     INNER JOIN DESCONTOFINANCEIROJUR D ON L.RECNUM = D.RECNUMBAIXA
-     WHERE L.DESCONTOFIN > 0
+    SELECT *
+      FROM (
+        SELECT B.CODFILIAL,
+               B.RECNUM,
+               B.VPAGO,
+               L.DESCONTOFIN AS VALOR,
+               B.DTCOMPETENCIA,
+               B.DTPAGTO,
+               B.DTCOMPENSACAO,
+               B.CODFILIALBANCO,
+               B.CODBANCO,
+               B.CODCONTABILBANCO,
+               B.CODCONTA,
+               B.GRUPOCONTA,
+               B.CODFORNEC,
+               '' AS CODCONTABCLIENTE,
+               B.TIPOPARCEIRO,
+               B.NUMTRANS,
+               'D' AS TIPO,
+               (
+                   CASE
+                       WHEN L.DUPLIC IS NULL
+                           OR L.DUPLIC = '0' THEN 'DESC FIN Nº ' || L.NUMNOTA || ' - ' || L.HISTORICO
+                       ELSE 'DESC FIN Nº ' || L.NUMNOTA || '-' || L.DUPLIC || ' - ' || L.HISTORICO
+                   END
+               ) HISTORICO
+          FROM PCLANC L
+         INNER JOIN BASEPCLANC B ON L.RECNUM = B.RECNUM
+         INNER JOIN DESCONTOFINANCEIROJUR D ON L.RECNUM = D.RECNUMBAIXA
+         WHERE L.DESCONTOFIN > 0
      /*SOMENTE REGISTROS COM DESCONTO FINANCEIRO*/
-       AND D.HISTORICO LIKE 'DESCONTO%'
+           AND D.HISTORICO LIKE 'DESCONTO%'
      /*PARA TERMOS SOMENTE O LANCAMENTO DE DESCONTO E NAO DE JUROS, IMPEDINDO DUPLICIDADE*/
+    )
+     GROUP BY CODFILIAL,
+              RECNUM,
+              VPAGO,
+              VALOR,
+              DTCOMPETENCIA,
+              DTPAGTO,
+              DTCOMPENSACAO,
+              CODFILIALBANCO,
+              CODBANCO,
+              CODCONTABILBANCO,
+              CODCONTA,
+              GRUPOCONTA,
+              CODFORNEC,
+              CODCONTABCLIENTE,
+              TIPOPARCEIRO,
+              NUMTRANS,
+              TIPO,
+              HISTORICO
     UNION ALL
 /*--DESCONTOS FINACEIROS INTEGRAIS OBTIDOS--*/
     SELECT L.CODFILIAL,
