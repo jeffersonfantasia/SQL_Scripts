09a851e8a0236f1438710cf3f99bc6a2539503da
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2021-03-23 22:21:53 -0300

    Update VIEW_JC_LANC_FISCAL.sql

--- Views/Lançamentos Fiscais/VIEW_JC_LANC_FISCAL.sql
+++ Views/Lançamentos Fiscais/VIEW_JC_LANC_FISCAL.sql
@@ -3,6 +3,48 @@ CREATE OR REPLACE VIEW VIEW_JC_LANC_FISCAL AS
 /*TODAS AS MOVIMENTACOES DA PCMOV PARA DESCONSIDERAR POSTERIORMENTE*/ (
         SELECT DISTINCT (NUMTRANSENT)
           FROM PCMOV
+    ),
+ /*RETIRAR REGISTROS DUPLICADOS*/
+     NFBASE AS (
+        SELECT B.NUMTRANSENT,
+               (
+                   CASE
+                       WHEN MIN (B.CODFISCAL) = 1933 THEN MIN (B.CODFISCAL)
+                       ELSE MAX (B.CODFISCAL)
+                   END
+               ) AS CODFISCAL,
+               B.VLCONTABIL,
+               (
+                   CASE
+                       WHEN MIN (TO_NUMBER (B.SITTRIBUT)) = 0 THEN MIN (B.SITTRIBUT)
+                       ELSE MAX (B.SITTRIBUT)
+                   END
+               ) AS CST_ICMS,
+               B.VLBASE,
+               B.ALIQUOTA,
+               B.VLICMS,
+               B.VLISENTAS
+          FROM PCNFBASE B
+         GROUP BY B.NUMTRANSENT,
+                  B.VLCONTABIL,
+                  B.VLBASE,
+                  B.ALIQUOTA,
+                  B.VLICMS,
+                  B.VLISENTAS
+    ),
+           /*RETIRAR REGISTROS DUPLICADOS*/
+     NFENTPISCOFINS AS (
+        SELECT P.NUMTRANSENT,
+               (
+                   CASE
+                       WHEN MIN (P.CODTRIBPISCOFINS) > 0 THEN MIN (P.CODTRIBPISCOFINS)
+                       ELSE MAX (P.CODTRIBPISCOFINS)
+                   END
+               ) AS CST_PISCOFINS,
+               MAX (P.VLPIS) AS VLPIS,
+               MAX (P.VLCOFINS) AS VLCOFINS
+          FROM PCNFENTPISCOFINS P
+         GROUP BY P.NUMTRANSENT
     ), NOTAS_DESPESAS AS (
         SELECT E.CODFILIALNF AS CODFILIAL,
                E.DTEMISSAO,
@@ -18,37 +60,38 @@ CREATE OR REPLACE VIEW VIEW_JC_LANC_FISCAL AS
                E.CODFORNEC,
                (E.ESPECIE || ' ' || E.NUMNOTA || ' - CNPJ: ' || E.CGC || ' - ' || E.FORNECEDOR) AS HISTORICO,
                B.VLCONTABIL AS VALOR,
-               B.SITTRIBUT AS CST_ICMS,
+               B.CST_ICMS,
                B.VLBASE,
                B.ALIQUOTA AS ALIQ_ICMS,
                B.VLICMS,
+               E.VLST,
                B.VLISENTAS,
                E.VLOUTRAS,
-               P.CODTRIBPISCOFINS AS CST_PISCOFINS,
+               P.CST_PISCOFINS,
                ROUND (P.VLPIS, 2) AS VLPIS,
                ROUND (P.VLCOFINS, 2) AS VLCOFINS
           FROM PCNFENT E
-         INNER JOIN PCNFBASE B ON E.NUMTRANSENT = B.NUMTRANSENT
-         INNER JOIN PCNFENTPISCOFINS P ON E.NUMTRANSENT = P.NUMTRANSENT
+          LEFT JOIN NFBASE B ON E.NUMTRANSENT = B.NUMTRANSENT
+          LEFT JOIN NFENTPISCOFINS P ON E.NUMTRANSENT = P.NUMTRANSENT
           LEFT JOIN PCMOV_NUMTRANSENT M ON E.NUMTRANSENT = M.NUMTRANSENT
          WHERE E.DTCANCEL IS NULL
-     /*NAO TRAZER NOTAS CANCELADAS*/
+        /*NAO TRAZER NOTAS CANCELADAS*/
            AND M.NUMTRANSENT IS NULL
-     /*NAO TRAZER NOTAS COM MOVIMENTACAO NA PCMOV*/
+        /*NAO TRAZER NOTAS COM MOVIMENTACAO NA PCMOV*/
            AND E.ESPECIE NOT IN (
             'OE', 'NE', 'TP'
         )
-     /*NÃO TRAZER NOTAS QUE SAO OUTRAS ESPECIES*/
+        /*NÃO TRAZER NOTAS QUE SAO OUTRAS ESPECIES*/
            AND NOT (E.ESPECIE = 'CT'
            AND B.CODFISCAL IN (
             2206
         ))
-     /*NÃO TRAZER ENTRADA NULACAO DE FRETE*/
+        /*NÃO TRAZER ENTRADA NULACAO DE FRETE*/
            AND NOT (E.ESPECIE = 'NF'
            AND B.CODFISCAL IN (
             1911, 1923, 2923, 1949, 2949
         ))
-     /*NÃO TRAZER REMESSAS*/
+  /*NÃO TRAZER REMESSAS*/
     )
     SELECT N.CODFILIAL,
            N.DTEMISSAO,
@@ -64,6 +107,7 @@ CREATE OR REPLACE VIEW VIEW_JC_LANC_FISCAL AS
            N.VLBASE,
            N.ALIQ_ICMS,
            N.VLICMS,
+           N.VLST,
            N.VLISENTAS,
            N.VLOUTRAS,
            N.CST_PISCOFINS,
