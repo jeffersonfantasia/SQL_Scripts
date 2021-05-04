e00f52c230f14c97687dc43d1e2f071c9284264a
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2021-05-04 16:31:40 -0300

    Create VIEW_JC_LANC_DIF_EMPRESA.sql

---
+++ Views/Contas a Pagar/VIEW_JC_LANC_DIF_EMPRESA.sql
@@ -0,0 +1,53 @@
+CREATE OR REPLACE VIEW VIEW_JC_LANC_DIF_EMPRESA AS
+    WITH BANCOS_EMPRESA AS (
+        SELECT B.CODBANCO,
+               (
+                   CASE
+                       WHEN B.CODFILIAL IN (
+                           1, 2, 7, 99
+                       ) THEN '1'
+                       ELSE B.CODFILIAL
+                   END
+               ) AS CODEMPRESA
+          FROM PCBANCO B
+    ), LANCAMENTOS_EMPRESA AS (
+        SELECT L.RECNUM,
+               (
+                   CASE
+                       WHEN L.CODFILIAL IN (
+                           1, 2, 7
+                       ) THEN '1'
+                       ELSE L.CODFILIAL
+                   END
+               ) AS CODEMPRESA
+          FROM PCLANC L
+         WHERE L.TIPOPARCEIRO = 'F'
+    )
+    SELECT B.CODFILIAL,
+           B.RECNUM,
+           B.VPAGO,
+           (
+               CASE
+                   WHEN B.VPAGO < 0 THEN (B.VPAGO * - 1)
+                   ELSE B.VPAGO
+               END
+           ) AS VALOR,
+           B.DTCOMPETENCIA,
+           B.DTPAGTO,
+           B.DTCOMPETENCIA AS DTCOMPENSACAO,
+           B.CODFILIALBANCO,
+           B.CODBANCO,
+           B.CODCONTABILBANCO,
+           B.CODCONTA,
+           B.GRUPOCONTA,
+           B.CODFORNEC,
+           '' AS CODCONTABCLIENTE,
+           B.TIPOPARCEIRO,
+           B.NUMTRANS
+      FROM PCLANC L
+     INNER JOIN VIEW_JC_LANC_BASE B ON L.RECNUM = B.RECNUM
+     INNER JOIN BANCOS_EMPRESA S ON B.CODBANCO = S.CODBANCO
+     INNER JOIN LANCAMENTOS_EMPRESA T ON B.RECNUM = T.RECNUM
+     WHERE S.CODEMPRESA <> T.CODEMPRESA
+     /*SOMENTE LANCAMENTO ENTRE EMPRESAS DIFERENTES*/;
+/
