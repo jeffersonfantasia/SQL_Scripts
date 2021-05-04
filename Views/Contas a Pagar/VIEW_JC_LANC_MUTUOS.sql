a68c5c0197194376baa2cdb78b667246983536d3
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2021-05-04 17:42:32 -0300

    Create VIEW_JC_LANC_MUTUOS.sql

---
+++ Views/Contas a Pagar/VIEW_JC_LANC_MUTUOS.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE VIEW VIEW_JC_LANC_MUTUOS AS
+    SELECT B.CODFILIAL,
+           B.RECNUM,
+           B.VPAGO,
+           B.VALOR,
+           B.DTCOMPETENCIA,
+           B.DTPAGTO,
+           B.DTCOMPENSACAO,
+           B.CODFILIALBANCO,
+           B.CODBANCO,
+           B.CODCONTABILBANCO,
+           B.CODCONTA,
+           B.GRUPOCONTA,
+           B.CODFORNEC,
+           '' AS CODCONTABCLIENTE,
+           B.TIPOPARCEIRO,
+           B.NUMTRANS,
+           'MT' AS TIPO,
+           (B.RECNUM || ' - ' || B.HISTORICO) AS HISTORICO
+      FROM VIEW_JC_LANC_BASE B
+     WHERE B.CODBANCO IN (
+        14, 15
+    )
+        /*SOMENTE BANCOS DOS MUTUOS*/
+       AND NOT ((B.CODBANCO = 14
+       AND B.CODCONTA = 300102)
+        OR (B.CODBANCO = 15
+       AND B.CODCONTA = 300101))
+     /*RETIRAR LANCAMENTOS DE APORTE E PAGAMENTO*/;
+/
