4827bc4a5b57fd6e292611ed02c06929d85d9dc5
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2021-07-16 17:21:53 -0300

    Update

--- Logística/Análise dos Inventários.sql
+++ Logística/Análise dos Inventários.sql
@@ -1,49 +1,49 @@
-SELECT NUMINVENT,
-       DATA,
-       CODFILIAL,
-       CODPROD,
-       DESCRICAO,
-       MARCA,
-       QTCONTADO,
-       QTESTGER,
-       QTAVARIA,
-       INVENTAVARIA,
-       CUSTO,
-       (QTCONTADO - QTESTGER) DIF,
-       (
-           CASE
-               WHEN (QTCONTADO - QTESTGER) > 0 THEN 'ENTRADA'
-               ELSE 'PERDA'
-           END
-       ) AS OPERACAO,
-       ((QTCONTADO - QTESTGER) * CUSTO) AS VLOPERACAO
-  FROM (
-    SELECT I.NUMINVENT,
-           I.DATA,
-           I.CODFILIAL,
-           I.CODPROD,
-           P.DESCRICAO,
-           M.MARCA,
-           SUM (I.QT1) AS QTCONTADO,
-           I.QTESTGER,
-           SUM (I.QTAVARIA1) AS QTAVARIA,
-           I.INVENTAVARIA,
-           E.CUSTOCONT AS CUSTO
-      FROM PCINVENTROT I
-      LEFT JOIN PCPRODUT P ON I.CODPROD = P.CODPROD
-      LEFT JOIN PCMARCA M ON P.CODMARCA = M.CODMARCA
-      LEFT JOIN PCEST E ON I.CODPROD = E.CODPROD
-       AND I.CODFILIAL = E.CODFILIAL
-     WHERE I.NUMINVENT = 1010
-     GROUP BY I.NUMINVENT,
-              I.DATA,
-              I.CODFILIAL,
-              I.CODPROD,
-              P.DESCRICAO,
-              M.MARCA,
-              I.QTESTGER,
-              I.INVENTAVARIA,
-              E.CUSTOCONT
-)
- WHERE QTCONTADO <> QTESTGER;
-/
+    SELECT NUMINVENT,
+           DATA,
+           CODFILIAL,
+           CODPROD,
+           DESCRICAO,
+           MARCA,
+           QTCONTADO,
+           QTESTGER,
+           QTAVARIA,
+           INVENTAVARIA,
+           CUSTO,
+           (QTCONTADO - QTESTGER) DIF,
+           (
+               CASE
+                   WHEN (QTCONTADO - QTESTGER) > 0 THEN 'ENTRADA'
+                   ELSE 'PERDA'
+               END
+           ) AS OPERACAO,
+           ((QTCONTADO - QTESTGER) * CUSTO) AS VLOPERACAO
+      FROM (
+        SELECT I.NUMINVENT,
+               I.DATA,
+               I.CODFILIAL,
+               I.CODPROD,
+               P.DESCRICAO,
+               M.MARCA,
+               SUM (I.QT1) AS QTCONTADO,
+               I.QTESTGER,
+               SUM (I.QTAVARIA1) AS QTAVARIA,
+               I.INVENTAVARIA,
+               E.CUSTOCONT AS CUSTO
+          FROM PCINVENTROT I
+          LEFT JOIN PCPRODUT P ON I.CODPROD = P.CODPROD
+          LEFT JOIN PCMARCA M ON P.CODMARCA = M.CODMARCA
+          LEFT JOIN PCEST E ON I.CODPROD = E.CODPROD
+           AND I.CODFILIAL = E.CODFILIAL
+         WHERE I.NUMINVENT = 1013
+         GROUP BY I.NUMINVENT,
+                  I.DATA,
+                  I.CODFILIAL,
+                  I.CODPROD,
+                  P.DESCRICAO,
+                  M.MARCA,
+                  I.QTESTGER,
+                  I.INVENTAVARIA,
+                  E.CUSTOCONT
+    )
+     WHERE QTCONTADO <> QTESTGER;
+    /
