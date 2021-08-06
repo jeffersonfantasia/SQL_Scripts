98080c652d9f6ddb63f1f2e4dec0c3713b527884
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2021-08-06 19:58:57 -0300

    Create VIEW_BI_VENDEDOR.sql

---
+++ Views/Cadastro Vendedores/VIEW_BI_VENDEDOR.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE VIEW VIEW_BI_VENDEDOR AS
+    SELECT U.CODUSUR,
+           U.USURDIRFV AS VENDEDOR,
+           U.CODSUPERVISOR,
+           V.NOME AS SUPERVISOR,
+           V.CODGERENTE,
+           G.NOMEGERENTE AS GERENTE,
+           (
+               CASE
+                   WHEN (U.BLOQUEIO = 'S'
+                      AND U.CODSUPERVISOR = 1) THEN 'OUTROS CAPITAL'
+                   WHEN (U.BLOQUEIO = 'S'
+                      AND U.CODSUPERVISOR = 2) THEN 'OUTROS CAPITAL'
+                   WHEN (U.BLOQUEIO = 'S'
+                      AND U.CODSUPERVISOR = 5) THEN 'OUTROS LOJA'
+                   ELSE U.USURDIRFV
+               END
+           ) AS VENDEDORES
+      FROM PCUSUARI U
+      LEFT JOIN PCSUPERV V ON V.CODSUPERVISOR = U.CODSUPERVISOR
+      LEFT JOIN PCGERENTE G ON V.CODGERENTE = G.CODGERENTE;
+/
