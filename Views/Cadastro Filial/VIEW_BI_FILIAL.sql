be53746463ac22bfbe8a8c725479a962747887b9
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2021-08-06 19:46:58 -0300

    Create VIEW_BI_FILIAL.sql

---
+++ Views/Cadastro Filial/VIEW_BI_FILIAL.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE VIEW VIEW_BI_FILIAL AS
+    SELECT CAST (CODIGO AS NUMBER) AS CODFILIAL,
+           (
+               CASE
+                   WHEN FANTASIA IS NULL THEN 'FILIAL 99'
+                   ELSE FANTASIA
+               END
+           ) AS FILIAIS,
+           (
+               CASE
+                   WHEN NOMEREMETENTE IS NULL THEN 'FILIAL 99'
+                   ELSE NOMEREMETENTE
+               END
+           ) AS EMPRESAS
+      FROM PCFILIAL
+     ORDER BY CODIGO;
+/
