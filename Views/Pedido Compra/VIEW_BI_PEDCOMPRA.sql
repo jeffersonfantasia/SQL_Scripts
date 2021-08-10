ae7787e21aed0ff36f82d59dd9281463e480fcc6
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2021-08-09 22:23:16 -0300

    Update VIEW_BI_PEDCOMPRA.sql

--- Views/Pedido Compra/VIEW_BI_PEDCOMPRA.sql
+++ Views/Pedido Compra/VIEW_BI_PEDCOMPRA.sql
@@ -1,27 +1,42 @@
 CREATE OR REPLACE VIEW VIEW_BI_PEDCOMPRA AS
-    SELECT P.CODFILIAL,
-           P.DATALANC AS DATA,
-           I.CODPROD,
-           I.QTPEDIDA,
-           (
-               CASE
-                   WHEN I.QTENTREGUE > I.QTPEDIDA THEN I.QTPEDIDA
-                   ELSE I.QTENTREGUE
-               END
-           ) AS QTENTREGUE,
-           (
-               CASE
-                   WHEN P.CODCOMPRADOR = 47 THEN 2
-                   WHEN (F.CODFORNECPRINC IN (
-                       2, 305, 9078, 8994, 9225
-                   )
-                      AND P.CODFILIAL = 2) THEN 1
-                   ELSE 3
-               END
-           ) AS CODCOMPRADOR,
-           P.TIPODESCARGA AS TIPOCOMPRA,
-           P.CODFORNEC
-      FROM PCITEM I
-      LEFT JOIN PCPEDIDO P ON I.NUMPED = P.NUMPED
-      LEFT JOIN PCFORNEC F ON P.CODFORNEC = F.CODFORNEC;
+    WITH PEDIDOCOMPRA AS (
+        SELECT P.CODFILIAL,
+               P.DATALANC AS DATA,
+               I.CODPROD,
+               I.QTPEDIDA AS QTCOMPRA,
+               (
+                   CASE
+                       WHEN I.QTENTREGUE > I.QTPEDIDA THEN I.QTPEDIDA
+                       ELSE I.QTENTREGUE
+                   END
+               ) AS QTENTREGUE,
+               (I.PLIQUIDO + I.VLIPI + I.VLST) AS PCOMPRA,
+               (
+                   CASE
+                       WHEN P.CODCOMPRADOR = 47 THEN 2
+                       WHEN (F.CODFORNECPRINC IN (
+                           2, 305, 9078, 8994, 9225
+                       )
+                          AND P.CODFILIAL = 2) THEN 1
+                       ELSE 3
+                   END
+               ) AS CODCOMPRADOR,
+               P.TIPODESCARGA AS TIPOCOMPRA,
+               P.CODFORNEC
+          FROM PCITEM I
+          LEFT JOIN PCPEDIDO P ON I.NUMPED = P.NUMPED
+          LEFT JOIN PCFORNEC F ON P.CODFORNEC = F.CODFORNEC
+    )
+    SELECT C.CODFILIAL,
+           C.DATA,
+           C.CODPROD,
+           C.QTCOMPRA,
+           C.QTENTREGUE,
+           C.PCOMPRA,
+           (C.QTCOMPRA * C.PCOMPRA) AS VLCOMPRA,
+           (C.QTENTREGUE * C.PCOMPRA) AS VLENTREGUE,
+           C.CODCOMPRADOR,
+           C.TIPOCOMPRA,
+           C.CODFORNEC
+      FROM PEDIDOCOMPRA C;
 /
