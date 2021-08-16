4e1378adf7a58886d915fbc74737afc1370f7251
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2021-08-16 20:54:52 -0300

    Create VIEW_BI_MARCA.sql

---
+++ Views/Cadastro Produtos/VIEW_BI_MARCA.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW VIEW_BI_MARCA AS
+    SELECT M.CODMARCA,
+           (
+               CASE
+                   WHEN M.CODMARCA IN (
+                       165, 166, 167
+                   ) THEN 'DOREL'
+                   ELSE M.MARCA
+               END
+           ) AS MARCA
+      FROM PCMARCA M
+     WHERE M.ATIVO = 'S'
+       AND EXISTS (
+        SELECT CODMARCA
+          FROM VIEW_BI_PRODUTO
+         WHERE CODMARCA = M.CODMARCA
+    );
+/
