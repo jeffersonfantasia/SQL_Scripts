e5f5288d3d23772c71d907cbc9e537e468aae113
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2021-11-19 16:24:25 -0300

    Create VIEW_JC_PREST_PERDAS.sql

---
+++ Views/Contas a Receber/VIEW_JC_PREST_PERDAS.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE VIEW VIEW_JC_PREST_PERDAS AS
+    SELECT T.CODDUPLIC,
+           T.CODFILIAL,
+           T.DATA,
+           T.DTVENC,
+           T.DTEMISSAO,
+           T.DUPLICATA,
+           T.CODBANCO,
+           T.CODCONTABILBANCO,
+           (
+               CASE
+                 /*PARA QUE NAO TRAGA INFORMACAO DE JUROS*/
+                   WHEN T.VPAGO > T.VALOR THEN T.VALOR
+                   ELSE T.VPAGO
+               END
+           ) VALOR,
+           T.CODCONTAB,
+           T.CODCLI,
+           T.CODCOB,
+           T.CODUSUR,
+           T.NUMTRANS,
+           T.CARTORIO,
+           T.PROTESTO,
+           'P' AS TIPO,
+           ('PERDA DUP ' || T.DUPLICATA || ' - ' || TRIM (T.CLIENTE)) AS HISTORICO
+      FROM VIEW_JC_BASE_PREST T
+     WHERE T.CODCOB = 'PERD';
+/
