21bc36168dbf6e7327506bc2d4cb6326f7aff40e
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2022-05-12 16:32:03 -0300

    Create VIEW_JC_PCMOV_SAID_IMP.sql

---
+++ Views/Movimentação de Produtos e Impostos/VIEW_JC_PCMOV_SAID_IMP.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE VIEW VIEW_JC_PCMOV_SAID_IMP AS
+SELECT DATAMES,
+       CODFILIAL,
+       TIPOCONTABIL,
+       SUM(VLICMS) AS VLTOTALICMS,
+       SUM(VLPIS) AS VLTOTALPIS,
+       SUM(VLCOFINS) AS VLTOTALCOFINS
+  FROM (SELECT LAST_DAY(M.DTMOV) AS DATAMES,
+               M.CODFILIAL,
+               'S' AS TIPOCONTABIL,
+               (CASE
+                 WHEN I.GERAICMSLIVROFISCAL = 'S' THEN
+                  ROUND(NVL((I.VLBASEICMS * I.PERCICM / 100), 0), 2)
+                 ELSE
+                  0
+               END) AS VLICMS,
+               ROUND(NVL(I.VLPIS, 0), 2) AS VLPIS,
+               ROUND(NVL(I.VLCOFINS, 0), 2) AS VLCOFINS
+          FROM PCMOV M
+          LEFT JOIN VIEW_JC_ITEM_NOTAFISCAL I ON M.NUMTRANSVENDA =
+                                                 I.NUMTRANSACAO
+                                             AND M.NUMSEQ = I.NUMSEQ
+                                             AND M.CODFILIALNF = I.CODFILIAL
+                                             AND M.CODPROD = I.CODPROD
+                                                /*AMARRACAO FUNDAMENTAL FECHAR O JOIN*/
+                                             AND I.TIPOMOV = 'S'
+         WHERE M.DTCANCEL IS NULL
+              /*TRAZER APENAS MOVIMENTAÇÕES CONTABEIS*/
+           AND M.STATUS IN ('A', 'AB')
+              /*RETIRAR NOTAS EMITIDAS ERRADAS*/
+           AND NOT (M.CODFISCAL IN (5927) AND M.CODFILIAL IN (3, 4)))
+ GROUP BY DATAMES, CODFILIAL, TIPOCONTABIL
+ ORDER BY CODFILIAL, DATAMES;
+/
