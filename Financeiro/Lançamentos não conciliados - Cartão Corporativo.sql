3a6896fb2c22b44880d4c18cf230671ab9d19063
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2022-07-12 17:06:46 -0300

    Update Lançamentos não conciliados - Cartão Corporativo.sql

--- Financeiro/Lançamentos não conciliados - Cartão Corporativo.sql
+++ Financeiro/Lançamentos não conciliados - Cartão Corporativo.sql
@@ -1,32 +1,28 @@
 SELECT M.DATA,
        M.CODBANCO,
        L.NUMNOTA,
-       --L.NUMTRANS,
-       (
-           CASE
-               WHEN M.CODCOB = 'D' THEN 'DINHEIRO'
-               ELSE M.CODCOB
-           END
-       ) MOEDA,
-       (
-           CASE
-               WHEN M.TIPO = 'C' THEN M.VALOR
-               ELSE M.VALOR * - 1
-           END
-       ) VALOR,
+       CASE
+         WHEN M.CODCOB = 'D' THEN 'DINHEIRO'
+         ELSE M.CODCOB
+       END MOEDA,
+       CASE
+         WHEN M.TIPO = 'C' THEN M.VALOR
+         ELSE M.VALOR * - 1
+       END VALOR,
        M.TIPO,
        M.HISTORICO,
-       (M.CODFUNC || ' - ' || (
-           SELECT NOME_GUERRA
-             FROM PCEMPR E
-            WHERE E.MATRICULA = M.CODFUNC
-       )) FUNC_LANC
+       (M.CODFUNC || ' - ' || (SELECT NOME_GUERRA
+                                 FROM PCEMPR E
+                                WHERE E.MATRICULA = M.CODFUNC
+                              )) FUNC_LANC
   FROM PCMOVCR M
   LEFT JOIN PCLANC L ON M.NUMTRANS = L.NUMTRANS
  WHERE M.CODBANCO = 41
    AND M.DTESTORNO IS NULL
    AND M.CONCILIACAO IS NULL
-   AND L.CODCONTA NOT IN (610103)
+   AND L.CODCONTA NOT IN (
+  610103
+)
  ORDER BY L.NUMNOTA,
           M.TIPO,
           L.HISTORICO;
