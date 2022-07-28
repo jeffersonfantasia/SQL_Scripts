26ccc95a1b9841796f714e6b730350d03762a862
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2022-07-28 16:05:43 -0300

    Update Análise custo 240 x 1103 x 201 - Filial 5 e 6.sql

--- Corporativo/Análise custo 240 x 1103 x 201 - Filial 5 e 6.sql
+++ Corporativo/Análise custo 240 x 1103 x 201 - Filial 5 e 6.sql
@@ -13,7 +13,8 @@ SELECT CODFILIAL,
        CUSTOPROXIMA_3,
        PBRUTO_FIL_3,
        PBRUTO_240,
-       PROXCOMPRA_240
+       PROXCOMPRA_240,
+       CUSTOFIN
   FROM (SELECT E.CODFILIAL,
                P.DTCADASTRO,
                P.CODFORNEC,
@@ -37,7 +38,8 @@ SELECT CODFILIAL,
                  WHERE CODPROD = E.CODPROD
                    AND CODFILIAL = '3') PBRUTO_FIL_3,
                ROUND((NVL(F.CUSTOREP, 0) * ((100 - F.PERCDESC) / 100)), 4) PBRUTO_240,
-               ROUND(NVL(E.CUSTOPROXIMACOMPRA, 0), 4) PROXCOMPRA_240
+               ROUND(NVL(E.CUSTOPROXIMACOMPRA, 0), 4) PROXCOMPRA_240,
+               ROUND (NVL (E.CUSTOFIN, 0), 4) CUSTOFIN
           FROM PCEST E, PCPRODUT P, PCPRODFILIAL F, PCFORNEC D
          WHERE P.CODFORNEC = D.CODFORNEC
            AND E.CODPROD = P.CODPROD
@@ -49,11 +51,57 @@ SELECT CODFILIAL,
            AND P.REVENDA = 'S'
            AND E.CODFILIAL IN ('5', '6')) C
  WHERE ENVIAFV_6 = 'S'
-   AND PBRUTO_240 <> PROXCOMPRA_240
-   AND (ABS(PBRUTO_240 - PROXCOMPRA_240) > 0.01 OR PROXCOMPRA_240 = 0)
+   AND ((PBRUTO_240 <> PROXCOMPRA_240 AND (ABS(PBRUTO_240 - PROXCOMPRA_240) > 0.01 OR PROXCOMPRA_240 = 0))
+   OR NVL(CUSTOFIN,0) = 0)
+   AND NVL(PROXCOMPRA_240,0) <> 0
  ORDER BY FORNECEDOR, CODPROD;
  /
- 
+
+--UPDATE--
+MERGE INTO PCEST E
+USING (SELECT CODFILIAL,
+              CODPROD,
+              DESCRICAO,
+              ENVIAFV_6,
+              PBRUTO_240,
+              PROXCOMPRA_240,
+              CUSTOFIN
+         FROM (SELECT E.CODFILIAL,
+                      E.CODPROD,
+                      P.DESCRICAO,
+                      (SELECT ENVIARFORCAVENDAS
+                         FROM PCPRODFILIAL
+                        WHERE CODPROD = F.CODPROD
+                          AND CODFILIAL = '6') ENVIAFV_6,
+                      ROUND((NVL(F.CUSTOREP, 0) * ((100 - F.PERCDESC) / 100)),
+                            4) PBRUTO_240,
+                      ROUND(NVL(E.CUSTOPROXIMACOMPRA, 0), 4) PROXCOMPRA_240,
+                      ROUND(NVL(E.CUSTOFIN, 0), 4) CUSTOFIN
+                 FROM PCEST E, PCPRODUT P, PCPRODFILIAL F, PCFORNEC D
+                WHERE P.CODFORNEC = D.CODFORNEC
+                  AND E.CODPROD = P.CODPROD
+                  AND F.CODPROD = P.CODPROD
+                  AND E.CODFILIAL = F.CODFILIAL
+                  AND P.CODPROD = P.CODPRODMASTER
+                  AND P.DTEXCLUSAO IS NULL
+                  AND P.TIPOMERC = 'L'
+                  AND P.REVENDA = 'S'
+                  AND E.CODFILIAL IN ('5', '6')) C
+        WHERE ENVIAFV_6 = 'S'
+          AND ((PBRUTO_240 <> PROXCOMPRA_240 AND
+              (ABS(PBRUTO_240 - PROXCOMPRA_240) > 0.01 OR
+              PROXCOMPRA_240 = 0)) OR NVL(CUSTOFIN, 0) = 0)
+          AND NVL(PROXCOMPRA_240, 0) <> 0) X
+ON (E.CODPROD = X.CODPROD AND E.CODFILIAL = X.CODFILIAL)
+WHEN MATCHED THEN
+  UPDATE
+     SET E.CUSTOREAL = X.PROXCOMPRA_240,
+         E.CUSTOFIN  = X.PROXCOMPRA_240,
+         E.CUSTOREP  = X.PROXCOMPRA_240,
+         E.CUSTOCONT = X.PROXCOMPRA_240;
+/
+
+
 SELECT CODFILIAL,
        DTCADASTRO,
        CODFORNEC,
@@ -120,7 +168,7 @@ SELECT CODFILIAL,
        AND P.TIPOMERC = 'L'
        AND P.REVENDA = 'S'
        AND E.CODFILIAL IN (
-        5, 6, 10
+        5, 6
     )
 ) C
  WHERE CODPROD IN (
