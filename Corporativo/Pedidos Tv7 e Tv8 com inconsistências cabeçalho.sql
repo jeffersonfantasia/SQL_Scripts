565fb0e9035b358bbcfcd934bacae891ee6a9768
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2022-08-14 00:13:03 -0300

    Update Pedidos Tv7 e Tv8 com inconsistências cabeçalho.sql

--- Corporativo/Pedidos Tv7 e Tv8 com inconsistências cabeçalho.sql
+++ Corporativo/Pedidos Tv7 e Tv8 com inconsistências cabeçalho.sql
@@ -9,20 +9,15 @@ SELECT C.DATA,
        C.CODEMITENTE
   FROM PCPEDC C
   LEFT JOIN PCCLIENT T ON C.CODCLI = T.CODCLI
- WHERE C.POSICAO NOT IN (
-    'F', 'C'
-)
-   AND C.CONDVENDA IN (
-    7, 8
-)
-   AND (C.CONTAORDEM IS NULL
-    OR (
-    CASE
-        WHEN (C.CONDVENDA = 8
-           AND C.CODCOB = 'SENT'
-           AND C.NUMPEDENTFUT IS NOT NULL) THEN 1
-        WHEN (C.CONDVENDA = 7
-           AND C.CODCOB <> 'SENT') THEN 1
-        ELSE 0
-    END
-) = 0)
+ WHERE C.POSICAO NOT IN ('F', 'C')
+   AND C.CONDVENDA IN (7, 8)
+   AND (C.CONTAORDEM IS NULL OR (CASE
+         WHEN (C.CONDVENDA = 8 AND C.CODCOB = 'SENT' AND
+              C.NUMPEDENTFUT IS NOT NULL) THEN
+          1
+         WHEN (C.CONDVENDA = 7 AND C.CODCOB <> 'SENT') THEN
+          1
+         ELSE
+          0
+       END) = 0);
+ /
