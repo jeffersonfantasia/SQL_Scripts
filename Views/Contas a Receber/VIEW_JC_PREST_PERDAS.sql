b4c595f94f7fd178a0325e4e08d519a45f70f553
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2022-08-27 14:03:29 -0300

    Update VIEW_JC_PREST_PERDAS.sql

--- Views/Contas a Receber/VIEW_JC_PREST_PERDAS.sql
+++ Views/Contas a Receber/VIEW_JC_PREST_PERDAS.sql
@@ -1,29 +1,30 @@
 CREATE OR REPLACE VIEW VIEW_JC_PREST_PERDAS AS
     SELECT T.CODDUPLIC,
-           T.CODFILIAL,
-           T.DATA,
-           T.DTVENC,
-           T.DTEMISSAO,
-           T.DUPLICATA,
-           T.CODBANCO,
-           T.CODCONTABILBANCO,
-           (
-               CASE
-                 /*PARA QUE NAO TRAGA INFORMACAO DE JUROS*/
-                   WHEN T.VPAGO > T.VALOR THEN T.VALOR
-                   ELSE T.VPAGO
-               END
-           ) VALOR,
-           T.CODCONTAB,
-           T.CODCLI,
-           T.CODCOB,
-           T.CODUSUR,
-           T.NUMTRANS,
-           T.NUMTRANSVENDA,
-           T.CARTORIO,
-           T.PROTESTO,
-           'P' AS TIPO,
-           ('PERDA DUP ' || T.DUPLICATA || ' - ' || TRIM (T.CLIENTE)) AS HISTORICO
-      FROM VIEW_JC_BASE_PREST T
-     WHERE T.CODCOB = 'PERD';
+            T.CODFILIAL,
+            T.DATA,
+            T.DTVENC,
+            T.DTEMISSAO,
+            T.DUPLICATA,
+            T.CODBANCO,
+            T.CODCONTABILBANCO,
+            CASE
+              WHEN VPAGO < 0 THEN
+               VPAGO * -1
+              WHEN T.VPAGO > T.VALOR THEN
+               T.VALOR --PARA QUE NAO TRAGA INFORMACAO DE JUROS
+              ELSE
+               T.VPAGO
+            END VALOR,
+            T.CODCONTAB,
+            T.CODCLI,
+            T.CODCOB,
+            T.CODUSUR,
+            T.NUMTRANS,
+            T.NUMTRANSVENDA,
+            T.CARTORIO,
+            T.PROTESTO,
+            'P' AS TIPO,
+            ('PERDA DUP ' || T.DUPLICATA || ' - ' || TRIM(T.CLIENTE)) AS HISTORICO
+       FROM VIEW_JC_BASE_PREST T
+      WHERE T.CODCOB = 'PERD';
 /
