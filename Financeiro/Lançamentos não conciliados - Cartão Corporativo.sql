d9f88281953c15e7420f6f96dfa8ca8eb8f0a3bf
Jefferson Fantasia <jefferson.fantasia@gmail.com>
2023-02-02 12:20:52 -0300

    Update Lançamentos não conciliados - Cartão Corporativo.sql

--- Financeiro/Lançamentos não conciliados - Cartão Corporativo.sql
+++ Financeiro/Lançamentos não conciliados - Cartão Corporativo.sql
@@ -1,28 +1,28 @@
-  SELECT M.DATA,
+SELECT M.DATA,
        M.CODBANCO,
        L.NUMNOTA,
        CASE
-         WHEN M.CODCOB = 'D' THEN 'DINHEIRO'
-         ELSE M.CODCOB
+         WHEN M.CODCOB = 'D' THEN
+          'DINHEIRO'
+         ELSE
+          M.CODCOB
        END MOEDA,
        CASE
-         WHEN M.TIPO = 'C' THEN M.VALOR
-         ELSE M.VALOR * - 1
+         WHEN M.TIPO = 'C' THEN
+          M.VALOR
+         ELSE
+          M.VALOR * -1
        END VALOR,
        M.TIPO,
        M.HISTORICO,
-       (M.CODFUNC || ' - ' || (SELECT NOME_GUERRA
-                                 FROM PCEMPR E
-                                WHERE E.MATRICULA = M.CODFUNC
-                              )) FUNC_LANC
+       --L.HISTORICO,
+       (M.CODFUNC || ' - ' ||
+       (SELECT NOME_GUERRA FROM PCEMPR E WHERE E.MATRICULA = M.CODFUNC)) FUNC_LANC
+--, L.rowid
   FROM PCMOVCR M
-  LEFT JOIN PCLANC L ON M.NUMTRANS = L.NUMTRANS
+  JOIN PCLANC L ON M.NUMTRANS = L.NUMTRANS
  WHERE M.CODBANCO = 41
    AND M.DTESTORNO IS NULL
    AND M.CONCILIACAO IS NULL
-   AND L.CODCONTA NOT IN (
-  610103
-)
- ORDER BY L.NUMNOTA,
-          M.TIPO,
-          L.HISTORICO;
+   AND L.CODCONTA NOT IN (610103)
+ ORDER BY L.NUMNOTA, M.TIPO, L.HISTORICO;
