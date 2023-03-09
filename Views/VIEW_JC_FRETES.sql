87bf7c066de9275e90f329c3386fbff79130301b
Jefferson Fantasia <jefferson.fantasia@gmail.com>
2023-03-09 21:21:51 +0000

    Update VIEW_JC_FRETES.sql

--- Views/VIEW_JC_FRETES.sql
+++ Views/VIEW_JC_FRETES.sql
@@ -1,27 +1,60 @@
-CREATE OR REPLACE VIEW VIEW_JC_FRETES AS
-    SELECT E.CODFILIAL,
+CREATE OR REPLACE VIEW VIEW_JC_CTES AS
+  WITH FRETES AS
+   (SELECT E.CODFILIAL,
            E.DTEMISSAO DTCTE,
-           (
-               CASE
-                   WHEN S.NUMTRANSVENDA IS NOT NULL THEN
-                       'S'
-                   WHEN E2.NUMTRANSENT IS NOT NULL THEN
-                       'E'
-                   ELSE
-                       'CT'
-               END
-           ) TIPO,
+           (CASE
+             WHEN S.NUMTRANSVENDA IS NOT NULL THEN
+              'S'
+             WHEN E2.NUMTRANSENT IS NOT NULL THEN
+              'E'
+             ELSE
+              'CT'
+           END) TIPO,
            E.NUMNOTA NUMNOTA_CTE,
            E.CODFORNEC,
-           T.FORNECEDOR,
+           T.FORNECEDOR TRANSPORTADORA,
            COALESCE(S.NUMTRANSVENDA, E2.NUMTRANSENT, 0) NUMTRANSACAO,
            COALESCE(S.CODCLI, E2.CODFORNEC, 10) CODCLI,
            ROUND((E.VLTOTAL / COUNT(E.NUMTRANSENT)
-                              OVER(PARTITION BY E.NUMTRANSENT)),
+                  OVER(PARTITION BY E.NUMTRANSENT)),
                  2) VLCTE
       FROM PCNFENT E
       JOIN PCFORNEC T ON T.CODFORNEC = E.CODFORNEC
       LEFT JOIN PCCONHECIMENTOFRETEI F ON F.NUMTRANSCONHEC = E.NUMTRANSENT
       LEFT JOIN PCNFSAID S ON S.CHAVENFE = F.CHAVENFE
       LEFT JOIN PCNFENT E2 ON E2.CHAVENFE = F.CHAVENFE
-     WHERE E.ESPECIE = 'CT' AND E.DTCANCEL IS NULL;
+     WHERE E.ESPECIE = 'CT'
+     -- AND E.DTEMISSAO >= TO_DATE('01/01/2017', 'DD/MM/YYYY')
+       AND E.DTCANCEL IS NULL)
+  SELECT CODFILIAL,
+         CASE
+           WHEN CODCLI = 10 THEN
+            DTCTE
+           ELSE
+            MIN(DTCTE) OVER(PARTITION BY NUMTRANSACAO)
+         END DATA,
+         TIPO,
+         NUMNOTA_CTE,
+         CODFORNEC,
+         CASE
+           WHEN CODCLI = 10 THEN
+            TRANSPORTADORA
+           ELSE
+            MIN(TRANSPORTADORA) OVER(PARTITION BY NUMTRANSACAO)
+         END TRANSPORTADORA,
+         NUMTRANSACAO,
+         CODCLI,
+         VLCTE,
+         CASE
+           WHEN CODCLI = 10 THEN
+            1
+           ELSE
+            COUNT(NUMTRANSACAO) OVER(PARTITION BY NUMTRANSACAO)
+         END QTCTESNOTA,
+         CASE
+           WHEN CODCLI = 10 THEN
+            VLCTE
+           ELSE
+            SUM(VLCTE) OVER(PARTITION BY NUMTRANSACAO)
+         END VLTOTALFRETE
+    FROM FRETES;
