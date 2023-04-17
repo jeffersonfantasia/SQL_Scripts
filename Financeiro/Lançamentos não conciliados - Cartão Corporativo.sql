da2d61a8a0e3fc3d6129abcd8c774694a1f01369
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2023-04-17 14:26:40 -0300

    UPdate

--- Financeiro/Lançamentos não conciliados - Cartão Corporativo.sql
+++ Financeiro/Lançamentos não conciliados - Cartão Corporativo.sql
@@ -1,28 +1,31 @@
-SELECT M.DATA,
-       M.CODBANCO,
-       L.NUMNOTA,
-       CASE
-         WHEN M.CODCOB = 'D' THEN
-          'DINHEIRO'
-         ELSE
-          M.CODCOB
-       END MOEDA,
-       CASE
-         WHEN M.TIPO = 'C' THEN
-          M.VALOR
-         ELSE
-          M.VALOR * -1
-       END VALOR,
-       M.TIPO,
-       M.HISTORICO,
-       --L.HISTORICO,
-       (M.CODFUNC || ' - ' ||
-       (SELECT NOME_GUERRA FROM PCEMPR E WHERE E.MATRICULA = M.CODFUNC)) FUNC_LANC
---, L.rowid
+SELECT DISTINCT M.DATA,
+                M.CODBANCO,
+                L.NUMNOTA,
+                --M.NUMCARR,
+                CASE
+                  WHEN M.CODCOB = 'D' THEN
+                   'DINHEIRO'
+                  ELSE
+                   M.CODCOB
+                END MOEDA,
+                CASE
+                  WHEN M.TIPO = 'C' THEN
+                   M.VALOR
+                  ELSE
+                   M.VALOR * -1
+                END VALOR,
+                M.TIPO,
+                M.HISTORICO,
+                --L.HISTORICO,
+                (M.CODFUNC || ' - ' ||
+                (SELECT NOME_GUERRA
+                    FROM PCEMPR E
+                   WHERE E.MATRICULA = M.CODFUNC)) FUNC_LANC
+                --,M.rowid
   FROM PCMOVCR M
   JOIN PCLANC L ON M.NUMTRANS = L.NUMTRANS
  WHERE M.CODBANCO = 41
    AND M.DTESTORNO IS NULL
    AND M.CONCILIACAO IS NULL
    AND L.CODCONTA NOT IN (610103)
- ORDER BY L.NUMNOTA, M.TIPO, L.HISTORICO;
+ ORDER BY L.NUMNOTA, M.TIPO, M.HISTORICO;
