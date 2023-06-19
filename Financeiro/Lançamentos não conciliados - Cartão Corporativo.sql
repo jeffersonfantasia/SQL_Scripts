64a1835f663753643587f46c11e65f1ad720378c
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2023-06-19 17:50:36 -0300

    Update

--- Financeiro/Lançamentos não conciliados - Cartão Corporativo.sql
+++ Financeiro/Lançamentos não conciliados - Cartão Corporativo.sql
@@ -1,11 +1,71 @@
+---ALTERAÇÃO NA PCLANC
 SELECT DISTINCT M.DATA,
+                L.DTCOMPETENCIA,
                 M.CODBANCO,
-								MIN(L.CODFORNEC) OVER (PARTITION BY NUMNOTA) CODFORNECPRINC,
-								L.CODFORNEC,
-								(SELECT F.FORNECEDOR FROM PCFORNEC F WHERE F.CODFORNEC = L.CODFORNEC) FORNECEDOR,
+                MIN(L.CODFORNEC) OVER(PARTITION BY NUMNOTA) CODFORNECPRINC,
+                L.CODFORNEC,
+                (SELECT F.FORNECEDOR
+                   FROM PCFORNEC F
+                  WHERE F.CODFORNEC = L.CODFORNEC) FORNECEDOR,
                 L.NUMNOTA,
                 M.NUMCARR,
-								(CASE WHEN (L.NUMNOTA = M.NUMCARR) THEN 'OK' ELSE 'NOT OK' END) VERNOTA,
+                (CASE
+                  WHEN (L.NUMNOTA = M.NUMCARR) THEN
+                   'OK'
+                  ELSE
+                   'NOT OK'
+                END) VERNOTA,
+                CASE
+                  WHEN M.CODCOB = 'D' THEN
+                   'DINHEIRO'
+                  ELSE
+                   M.CODCOB
+                END MOEDA,
+                CASE
+                  WHEN M.TIPO = 'C' THEN
+                   M.VALOR
+                  ELSE
+                   M.VALOR * -1
+                END VALOR,
+                M.TIPO,
+                M.HISTORICO HISTORICO_PCMOVCR,
+                L.HISTORICO,
+                (CASE
+                  WHEN (L.HISTORICO = M.HISTORICO) THEN
+                   'OK'
+                  ELSE
+                   'NOT OK'
+                END) VERHISTORICO,
+                (M.CODFUNC || ' - ' ||
+                (SELECT NOME_GUERRA
+                    FROM PCEMPR E
+                   WHERE E.MATRICULA = M.CODFUNC)) FUNC_LANC,
+                L.rowid
+  FROM PCLANC L
+  JOIN PCMOVCR M ON M.NUMTRANS = L.NUMTRANS
+ WHERE M.CODBANCO = 41
+   AND M.DTESTORNO IS NULL
+   AND M.CONCILIACAO IS NULL
+   AND L.CODCONTA NOT IN (610103)
+ ORDER BY L.NUMNOTA, M.TIPO, M.HISTORICO;
+
+---ALTERAÇÃO NA PCMOVCR
+SELECT DISTINCT M.DATA,
+                L.DTCOMPETENCIA,
+                M.CODBANCO,
+                MIN(L.CODFORNEC) OVER(PARTITION BY NUMNOTA) CODFORNECPRINC,
+                L.CODFORNEC,
+                (SELECT F.FORNECEDOR
+                   FROM PCFORNEC F
+                  WHERE F.CODFORNEC = L.CODFORNEC) FORNECEDOR,
+                L.NUMNOTA,
+                M.NUMCARR,
+                (CASE
+                  WHEN (L.NUMNOTA = M.NUMCARR) THEN
+                   'OK'
+                  ELSE
+                   'NOT OK'
+                END) VERNOTA,
                 CASE
                   WHEN M.CODCOB = 'D' THEN
                    'DINHEIRO'
@@ -20,21 +80,22 @@ SELECT DISTINCT M.DATA,
                 END VALOR,
                 M.TIPO,
                 M.HISTORICO,
-                L.HISTORICO,
-								(CASE WHEN (L.HISTORICO = M.HISTORICO) THEN 'OK' ELSE 'NOT OK' END) VERHISTORICO,
+                L.HISTORICO HISTORICO_PCLANC,
+                (CASE
+                  WHEN (L.HISTORICO = M.HISTORICO) THEN
+                   'OK'
+                  ELSE
+                   'NOT OK'
+                END) VERHISTORICO,
                 (M.CODFUNC || ' - ' ||
                 (SELECT NOME_GUERRA
                     FROM PCEMPR E
-                   WHERE E.MATRICULA = M.CODFUNC)) FUNC_LANC
- --,M.rowid
- ,L.rowid
-  --FROM PCMOVCR M
-  --JOIN PCLANC L ON M.NUMTRANS = L.NUMTRANS
-FROM PCLANC L 
-JOIN PCMOVCR M ON M.NUMTRANS = L.NUMTRANS
+                   WHERE E.MATRICULA = M.CODFUNC)) FUNC_LANC,
+                M.rowid
+  FROM PCMOVCR M
+  JOIN PCLANC L ON M.NUMTRANS = L.NUMTRANS
  WHERE M.CODBANCO = 41
    AND M.DTESTORNO IS NULL
-   --AND M.CONCILIACAO IS NULL
-	 AND L.NUMNOTA IN (1209421)
+   AND M.CONCILIACAO IS NULL
    AND L.CODCONTA NOT IN (610103)
  ORDER BY L.NUMNOTA, M.TIPO, M.HISTORICO;
