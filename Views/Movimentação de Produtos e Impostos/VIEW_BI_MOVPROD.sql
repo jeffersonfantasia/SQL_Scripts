7a62f937b68113bba83e8a414d9d00b04c0af60d
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-01-07 14:03:05 -0300

    Update VIEW_BI_MOVPROD.sql

--- Views/Movimentação de Produtos e Impostos/VIEW_BI_MOVPROD.sql
+++ Views/Movimentação de Produtos e Impostos/VIEW_BI_MOVPROD.sql
@@ -1,30 +1,35 @@
 CREATE OR REPLACE VIEW VIEW_BI_MOVPROD AS
     SELECT I.DATA,
-           CAST (I.CODFILIAL AS NUMBER) AS CODFILIAL,
+           CAST(I.CODFILIAL AS NUMBER) AS CODFILIAL,
            I.NUMTRANSACAO,
            I.CODPROD,
            I.QTCONT AS QT,
            I.PUNITCONT AS VLPROD,
-           ROUND (I.CUSTOFINEST, 4) AS CUSTO,
+           ROUND(I.CUSTOFINEST, 4) AS CUSTO,
            I.VLPRODUTO AS VLTOTPROD,
-           (
-               CASE
-                   WHEN I.TIPOMOV = 'E' THEN ROUND (((I.QTCONT * I.CUSTOFINEST) + I.VLICMS + I.VLPIS + I.VLCOFINS), 2)
-                   ELSE ROUND (((I.QTCONT * I.CUSTOFINEST) + I.VLICMS + I.VLPIS + I.VLCOFINS + I.VLICMSPARTILHA), 2)
-               END
-           ) AS VLCMV,
+           (CASE
+             WHEN I.TIPOMOV = 'E' THEN
+              ROUND(((I.QTCONT * I.CUSTOFINEST) + I.VLICMS + I.VLPIS +
+                    I.VLCOFINS),
+                    2)
+             ELSE
+              ROUND(((I.QTCONT * I.CUSTOFINEST) + I.VLICMS + I.VLPIS +
+                    I.VLCOFINS + I.VLICMSPARTILHA),
+                    2)
+           END) AS VLCMV,
            I.VLFRETE,
            I.VLST,
            I.VLOUTRASDESP,
            I.VLCONTABIL AS VLTOTALNF,
-           NVL (I.CODUSUR, 0) AS CODUSUR,
+           NVL(I.CODUSUR, 0) AS CODUSUR,
            I.TIPOMOV,
-           CAST (I.CODFISCAL AS VARCHAR2 (4)) AS CODFISCAL,
+           CAST(I.CODFISCAL AS VARCHAR2(4)) AS CODFISCAL,
            I.CODFORCLI,
+           I.PARCELAS,
            I.PRAZO,
-					 I.CODCOB
+           I.CODCOB
       FROM VIEW_JC_ITEM_NOTAFISCAL I
      WHERE I.DTCANCEL IS NULL
        AND I.CODFISCAL IS NOT NULL
        AND I.NUMTRANSACAO IS NOT NULL
-       AND I.CODFILIAL NOT IN ( 3, 4 );
+       AND I.CODFILIAL NOT IN (3, 4);
