35b212ad470162f668d258f6017230e8adcb4d42
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-05-29 17:27:43 -0300

    Create DITO_VENDEDORES.sql

---
+++ Views/Dito_CRM/DITO_VENDEDORES.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE VIEW BROKER.DITO_VENDEDORES AS
+/**********************************************************
+Trazer vendedores que são:
+1. Do Gerente 3 LOJA ou vendedor 37 ECOMMERCE
+2. Que não possuem DIRETORIA no nome
+3. Que estão ativos ou estão inativo a partir de 01/01/2022
+**********************************************************/
+
+SELECT U.NOME NOME_VENDEDOR,
+       U.CODUSUR CODIGO_VENDEDOR,
+       U.CPF CPF_VENDEDOR,
+       TO_NUMBER(U.CODFILIAL) ID_LOJA,
+       F.FANTASIA LOJA,
+       DECODE(U.BLOQUEIO, 'S', 'INATIVO', 'ATIVO') STATUS,
+       (CASE
+         WHEN U.CODUSUR = S.COD_CADRCA THEN
+          'GERENTE'
+         ELSE
+          'VENDEDOR'
+       END) CARGO
+  FROM PCUSUARI U
+  LEFT JOIN PCFILIAL F ON F.CODIGO = U.CODFILIAL
+  LEFT JOIN PCSUPERV S ON S.CODSUPERVISOR = U.CODSUPERVISOR
+ WHERE (U.CODUSUR = 37 OR S.CODGERENTE = 3)
+   AND INSTR(U.NOME, 'DIRETORIA') = 0
+   AND NVL(U.DTTERMINO, TO_DATE('01/01/2022','DD/MM/YYYY')) >= TO_DATE('01/01/2022','DD/MM/YYYY');
+
