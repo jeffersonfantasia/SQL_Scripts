cdec5b0ed755e6baff24a9350ffaace913be8ad9
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-06-01 22:00:00 -0300

    Update DITO_CLIENTES.sql

--- Views/Dito_CRM/DITO_CLIENTES.sql
+++ Views/Dito_CRM/DITO_CLIENTES.sql
@@ -5,36 +5,35 @@ Trazer clientes que são:
 3. Que possuem email preenchido
 4. Clientes que não estajam como excluidos
 5. Que não possuem TESTE ou CONTATO no nome ou no EMAIL
+6. Para clientes que nunca compraram trazer somente os cadastrados em 2022 para frente
+7. Para os clientes que compraram trazer apenas os que tem compra de 2022 para frente
 
 Tratamento dos campos:
 1. Trazer o NOME da JCCLIENTCLUB, caso nulo, trazer primeiro nome da PCCLIENT.CLIENTE
 2. Trazer o SOBRENOME da JCCLIENTCLUB, caso nulo, trazer demais nomes da PCCLIENT.CLIENTE
 3. Trazer o nome da cidade da tabela PCCIDADE pois na PCCLIENT.MUNICENT o nome esta cortado
+4. Para clientes cadastrados nas lojas, trazer CIDADE, UF, CEP nulos
 **********************************************************/
 WITH CEP_LOJAS AS
  (SELECT REGEXP_REPLACE(F.CEP, '[^0-9]', '') CEP_LOJA
     FROM PCFILIAL F
    WHERE INSTR(F.FANTASIA, 'LOJA') > 0),
-CLIENTES AS
+
+CLIENTES_FILTRADOS AS
  (SELECT C.CODCLI CODIGO_CLIENTE,
          REGEXP_REPLACE(C.CGCENT, '[^0-9]', '') CPF_CNPJ,
          C.DTCADASTRO DATA_CADASTRO,
          NVL(C.DTULTALTER, C.DTCADASTRO) DATA_UPDATE,
          DECODE(C.TIPOFJ, 'J', 'Jurídica', 'F', 'Física', 'Física') TIPO_PESSOA,
          C.CLIENTE,
-         (CASE
-           WHEN INSTR(TRIM(C.CLIENTE), ' ') > 0 THEN
-            UPPER(SUBSTR(TRIM(C.CLIENTE), 1, INSTR(TRIM(C.CLIENTE), ' ') - 1))
-           ELSE
-            UPPER(TRIM(C.CLIENTE))
+         (CASE WHEN INSTR(TRIM(C.CLIENTE), ' ') > 0
+            THEN UPPER(SUBSTR(TRIM(C.CLIENTE), 1, INSTR(TRIM(C.CLIENTE), ' ') - 1))
+            ELSE UPPER(TRIM(C.CLIENTE))
          END) PRIMEIRO_NOME_PCCLIENT,
-         CASE
-           WHEN INSTR(TRIM(C.CLIENTE), ' ') > 0 THEN
-            UPPER(TRIM(SUBSTR(TRIM(C.CLIENTE),
-                              INSTR(TRIM(C.CLIENTE), ' ') + 1)))
-           ELSE
-            NULL
-         END AS SOBRENOME_PCCLIENT,
+         (CASE WHEN INSTR(TRIM(C.CLIENTE), ' ') > 0
+            THEN UPPER(TRIM(SUBSTR(TRIM(C.CLIENTE),INSTR(TRIM(C.CLIENTE), ' ') + 1)))
+            ELSE NULL
+         END) SOBRENOME_PCCLIENT,
          TRIM(B.NOME) NOME_JCCLIENTCLUB,
          TRIM(B.SOBRENOME) SOBRENOME_JCCLIENTCLUB,
          LOWER(C.EMAIL) EMAIL,
@@ -44,38 +43,14 @@ CLIENTES AS
          D.NOMECIDADE CIDADE,
          C.ESTENT ESTADO,
          REGEXP_REPLACE(C.CEPENT, '[^0-9]', '') CEP,
-         C.DTEXCLUSAO,
-         '' Qtd_Filhos_Netos,
-         B.ONDECONHECEU Onde_Conheceu,
-         '' Nome_Filho_1,
-         '' Data_Nascimento_Filho_1,
-         '' Genero_Filho_1,
-         '' Nome_Filho_2,
-         '' Data_Nascimento_Filho_2,
-         '' Genero_Filho_2,
-         '' Nome_Filho_3,
-         '' Data_Nascimento_Filho_3,
-         '' Genero_Filho_3,
-         '' Nome_Filho_4,
-         '' Data_Nascimento_Filho_4,
-         '' Gênero_Filho_4,
-         '' Nome_Filho_5,
-         '' Data_Nascimento_Filho_5,
-         '' Genero_Filho_5,
-         '' Categoria_Cliente,
-         (CASE
-           WHEN (C.CODCLI = B.CODCLI) THEN
-            'LOJA'
-           ELSE
-            'SITE'
-         END) Origem_Cadastro,
-         '' Pontuacao_Cliente,
-         '' Retirou_Mimo_Mes_Atual,
-         '' Retirou_Cinema_Mes_Atual,
-         '' Retirou_Teatro_Mes_Atual,
-         '' Retirou_Brinde_Mes_Atual , 
-				 (SELECT COUNT(P.CODCLI) FROM PCPEDC P WHERE P.CODCLI = C.CODCLI) VENDAS,
-				 EXTRACT (YEAR FROM C.DTCADASTRO) ANO
+         (CASE WHEN (C.CODCLI = B.CODCLI)
+            THEN 'LOJA'
+            ELSE 'SITE'
+         END) ORIGEM_CADASTRO,
+         B.ONDECONHECEU ONDE_CONHECEU,
+         (SELECT COUNT(P.CODCLI) FROM PCPEDC P WHERE P.CODCLI = C.CODCLI) VENDAS,
+         EXTRACT(YEAR FROM (SELECT MAX(DATA) FROM PCPEDC P WHERE P.CODCLI = C.CODCLI)) ANO_ULTVENDA,
+         EXTRACT(YEAR FROM C.DTCADASTRO) ANO_CADASTRO
     FROM PCCLIENT C
     JOIN DITO_VENDEDORES V ON V.CODIGO_VENDEDOR = C.CODUSUR1
     LEFT JOIN JCCLIENTCLUB B ON B.CODCLI = C.CODCLI
@@ -91,13 +66,16 @@ CLIENTES AS
      AND INSTR(UPPER(C.EMAIL), 'SHOPEE.COM') = 0
      AND INSTR(UPPER(C.EMAIL), 'EMAIL.COM.BR') = 0
      AND INSTR(UPPER(C.EMAIL), 'WALMART') = 0
-		 AND INSTR(UPPER(C.EMAIL), 'MARKETPLACE') = 0
-		 AND INSTR(UPPER(C.EMAIL), 'BROKERDISTRIBUI') = 0
+     AND INSTR(UPPER(C.EMAIL), 'MARKETPLACE') = 0
+     AND INSTR(UPPER(C.EMAIL), 'BROKERDISTRIBUI') = 0
+     AND INSTR(UPPER(C.EMAIL), 'INTEGRACOMMERCE') = 0
      AND INSTR(UPPER(C.EMAIL), 'NULL') = 0
      AND C.EMAIL IS NOT NULL),
-RESULTADO AS
+
+CLIENTES_TRATADOS AS
  (SELECT VENDAS,
-         ANO,
+         ANO_CADASTRO,
+         ANO_ULTVENDA,
          CODIGO_CLIENTE,
          CPF_CNPJ,
          DATA_CADASTRO,
@@ -109,105 +87,60 @@ RESULTADO AS
          CELULAR,
          SEXO,
          LENGTH(CELULAR) TAMNHO_CEL,
-         (CASE
-           WHEN CEP IN (SELECT CEP_LOJA FROM CEP_LOJAS) THEN
-            NULL
-           ELSE
-            CIDADE
+         (CASE WHEN CEP IN (SELECT CEP_LOJA FROM CEP_LOJAS)
+            THEN NULL
+            ELSE CIDADE
          END) CIDADE,
-         (CASE
-           WHEN CEP IN (SELECT CEP_LOJA FROM CEP_LOJAS) THEN
-            NULL
-           ELSE
-            ESTADO
+         (CASE WHEN CEP IN (SELECT CEP_LOJA FROM CEP_LOJAS)
+            THEN NULL
+            ELSE ESTADO
          END) ESTADO,
-         (CASE
-           WHEN CEP IN (SELECT CEP_LOJA FROM CEP_LOJAS) THEN
-            NULL
-           ELSE
-            CEP
+         (CASE WHEN CEP IN (SELECT CEP_LOJA FROM CEP_LOJAS)
+            THEN NULL
+            ELSE CEP
          END) CEP,
-         Origem_Cadastro,
-         Onde_Conheceu,
-         Qtd_Filhos_Netos,
-         Nome_Filho_1,
-         Data_Nascimento_Filho_1,
-         Genero_Filho_1,
-         Nome_Filho_2,
-         Data_Nascimento_Filho_2,
-         Genero_Filho_2,
-         Nome_Filho_3,
-         Data_Nascimento_Filho_3,
-         Genero_Filho_3,
-         Nome_Filho_4,
-         Data_Nascimento_Filho_4,
-         Gênero_Filho_4,
-         Nome_Filho_5,
-         Data_Nascimento_Filho_5,
-         Genero_Filho_5,
-         Categoria_Cliente,
-         Pontuacao_Cliente,
-         Retirou_Mimo_Mes_Atual,
-         Retirou_Cinema_Mes_Atual,
-         Retirou_Teatro_Mes_Atual,
-         Retirou_Brinde_Mes_Atual
-    FROM CLIENTES
-   WHERE LENGTH(CPF_CNPJ) = 11)
+         ORIGEM_CADASTRO,
+         ONDE_CONHECEU
+    FROM CLIENTES_FILTRADOS)
 
---AND CODIGO_CLIENTE = 555392
---AND SOBRENOME_JCCLIENTCLUB IS NOT NULL
-
-SELECT COUNT(*) OVER() total, C.*
-  FROM RESULTADO C
-	WHERE NOT(VENDAS = 0 AND ANO <= 2022)
-
-
-
---AND ROWNUM <= 5.000
-;
-;
-/****************************************************************
-SELECT * FROM JCCLUBCLIENTE WHERE CADASTRADO = 'N' AND CPF IS NOT NULL AND EMAIL IS NOT NULL;
-SELECT * FROM JCCLUB;
-SELECT J.* FROM JCCLIENTCLUB J;
- SELECT CODCLI, COUNT(*)  FROM JCCLIENTCLUB J GROUP BY CODCLI;
-
-
-
-SELECT C.CODCLI CODIGO_CLIENTE,
-       C.CGCENT CPF_CNPJ,
-       LENGTH(C.CGCENT) TAMANHO,
-       C.*
-FROM PCCLIENT C
-JOIN DITO_VENDEDORES V ON V.CODIGO_VENDEDOR = C.CODUSUR1
-WHERE LENGTH(C.CGCENT) = 9
---LEFT JOIN JCCLIENTCLUB B ON B.CODCLI = C.CODCLI
-
-;
-
-
-SELECT * FROM PCCLIENT C WHERE C.CODCLI = 149623;
-
-
-SELECT SOBRENOME, COUNT(codcli) FROM JCCLIENTCLUB GROUP BY SOBRENOME;
-
-SELECT ROWID, j.* FROM JCCLIENTCLUB j WHERE CODCLI IN (
-557070,
-557086,
-557108,
-557138,
-557143,
-556791,
-556836);
-
-
-SELECT c.dtcadastro, B.SOBRENOME,
-       C.*
-  FROM PCCLIENT C
-  LEFT JOIN JCCLIENTCLUB B ON C.CODCLI = B.CODCLI
- WHERE C.CODATV1 = 25
-   AND c.dtcadastro IS NOT NULL
-   AND INSTR(C.CLIENTE, 'CADASTRO') > 0
- ORDER BY c.dtcadastro DESC;
- 
- **********/
+SELECT CODIGO_CLIENTE,
+       CPF_CNPJ,
+       DATA_CADASTRO,
+       DATA_UPDATE,
+       TIPO_PESSOA,
+       NOME,
+       SOBRENOME,
+       EMAIL,
+       CELULAR,
+       SEXO,
+       CIDADE,
+       ESTADO,
+       CEP,
+       ORIGEM_CADASTRO,
+       ONDE_CONHECEU,
+       '' QTD_FILHOS_NETOS,
+       '' NOME_FILHO_1,
+       '' DATA_NASCIMENTO_FILHO_1,
+       '' GENERO_FILHO_1,
+       '' NOME_FILHO_2,
+       '' DATA_NASCIMENTO_FILHO_2,
+       '' GENERO_FILHO_2,
+       '' NOME_FILHO_3,
+       '' DATA_NASCIMENTO_FILHO_3,
+       '' GENERO_FILHO_3,
+       '' NOME_FILHO_4,
+       '' DATA_NASCIMENTO_FILHO_4,
+       '' GÊNERO_FILHO_4,
+       '' NOME_FILHO_5,
+       '' DATA_NASCIMENTO_FILHO_5,
+       '' GENERO_FILHO_5,
+       '' CATEGORIA_CLIENTE,
+       '' PONTUACAO_CLIENTE,
+       '' RETIROU_MIMO_MES_ATUAL,
+       '' RETIROU_CINEMA_MES_ATUAL,
+       '' RETIROU_TEATRO_MES_ATUAL,
+       '' RETIROU_BRINDE_MES_ATUAL
+  FROM CLIENTES_TRATADOS C
+ WHERE LENGTH(CPF_CNPJ) = 11
+   AND NOT(VENDAS = 0 AND ANO_CADASTRO <= 2022)
+   AND NOT(VENDAS > 0 AND ANO_ULTVENDA <= 2022);
