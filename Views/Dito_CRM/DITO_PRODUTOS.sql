a28ee0417a6d830fe4a3b1300ff07d9dcfda67fa
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-06-03 17:19:22 -0300

    Create DITO_PRODUTOS.sql

---
+++ Views/Dito_CRM/DITO_PRODUTOS.sql
@@ -0,0 +1,37 @@
+/**********************************************************
+Trazer clientes que são:
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+7. 
+
+Tratamento dos campos:
+1. 
+2. 
+3. 
+4. 
+5. 
+
+CODIGO_CLIENTE,
+CPF_CNPJ,
+DATA_COMPRA,
+ID_TRANSACAO,
+ID_PRODUTO,
+NOME_PRODUTO,
+QUANTIDADE,
+PRECO_PRODUTO,
+VALOR_UNITARIO,
+OPERACAO_PRODUTO,
+DEPARTAMENTO,
+CATEGORIA,
+SUB_CATEGORIA,
+COLECAO,
+COR,
+TAMANHO,
+IDADE_SUGERIDA
+
+**********************************************************/
+
