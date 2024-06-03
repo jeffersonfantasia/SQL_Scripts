b35e0eebf3b65846c8929cf77002eaacf1b7488d
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-06-03 12:38:12 -0300

    Create DITO_TRANSACOES.sql

---
+++ Views/Dito_CRM/DITO_TRANSACOES.sql
@@ -0,0 +1,34 @@
+/**********************************************************
+Trazer clientes que são:
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+7. 
+
+Tratamento dos campos:
+1. 
+2. 
+3. 
+4. 
+5. 
+**********************************************************/
+
+CODIGO_CLIENTE,
+TO_CHAR(LPAD(CPF_CNPJ,11,0)) AS CPF_CNPJ,
+DATA_COMPRA,
+ID_TRANSACAO,
+NOME_LOJA,
+ID_LOJA,
+NOME_VENDEDOR,
+CODIGO_VENDEDOR,
+METODO_PAGAMENTO,
+TOTAL,
+TIPO_LOJA,
+QUANTIDADE_PRODUTOS,
+TOTAL_DESCONTO,
+TOTAL_FRETE,
+CUPOM,
+CLASSIFICACAO_LOJA
