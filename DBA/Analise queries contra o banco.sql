3ebfbdaed10997e4dbf56601c123edf7d31f20d2
Jefferson Fantasia <57639040+jeffersonfantasia@users.noreply.github.com>
2024-06-25 18:30:59 -0300

    Create Analise queries contra o banco.sql

---
+++ DBA/Analise queries contra o banco.sql
@@ -0,0 +1,52 @@
+----análise ultimas queries
+WITH SUMMARY_SNAP AS
+ (SELECT S.SQL_ID,
+         MAX(T.SNAP_TIME) SNAP_TIME
+    FROM PERFSTAT.STATS$SQL_SUMMARY S
+    JOIN PERFSTAT.STATS$SNAPSHOT T ON T.SNAP_ID = S.SNAP_ID
+   GROUP BY S.SQL_ID)
+SELECT *
+  FROM (SELECT S.SNAP_TIME, V.LAST_ACTIVE_TIME,
+               V.SQL_ID,
+               V.MODULE,
+               V.PARSING_SCHEMA_NAME,
+               V.SERVICE,
+               V.SQL_TEXT,
+               V.SQL_FULLTEXT,
+               V.CPU_TIME,
+               V.BUFFER_GETS,
+               ROUND(((V.ELAPSED_TIME + 1) / (V.EXECUTIONS + 1) / 1000000),2) SEGUNDOS
+          FROM V$SQL V
+          JOIN SUMMARY_SNAP S ON S.SQL_ID = V.SQL_ID
+         WHERE S.SNAP_TIME >= TRUNC(SYSDATE) - 1
+           AND V.CHILD_NUMBER = 0)
+ ORDER BY LAST_ACTIVE_TIME DESC;
+
+----análise das queries mais demoradas
+WITH SUMMARY_SNAP AS
+ (SELECT S.SQL_ID, S.
+         MAX(T.SNAP_TIME) SNAP_TIME
+    FROM PERFSTAT.STATS$SQL_SUMMARY S
+    JOIN PERFSTAT.STATS$SNAPSHOT T ON T.SNAP_ID = S.SNAP_ID
+   GROUP BY S.SQL_ID),
+CONSULTA AS (
+ SELECT S.SNAP_TIME,
+ V.LAST_ACTIVE_TIME,
+               V.SQL_ID,
+               V.MODULE,
+               V.PARSING_SCHEMA_NAME SCHEMA_NAME,
+               V.SERVICE,
+               V.SQL_TEXT,
+               V.SQL_FULLTEXT,
+               V.CPU_TIME,
+               V.BUFFER_GETS,
+							 V.ELAPSED_TIME,
+							 V.EXECUTIONS--,
+               --ROUND(((V.ELAPSED_TIME ) / (V.EXECUTIONS + 1 ) / 1000000),2) SEGUNDOS
+          FROM V$SQL V
+          JOIN SUMMARY_SNAP S ON S.SQL_ID = V.SQL_ID
+         WHERE S.SNAP_TIME >= TRUNC(SYSDATE) - 1
+           AND V.CHILD_NUMBER = 0)
+ SELECT *
+ FROM CONSULTA
+ ORDER BY SEGUNDOS DESC;
